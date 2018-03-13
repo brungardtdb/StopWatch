e0e3fcc9d1ba626cddbab90eb5a178737fc40770
David Brungardt <34960989+brungardtdb@users.noreply.github.com>
2018-03-12 20:20:52 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+    static void Main()
+    {
+        var timer = new Timer();
+
+        // Prompt user to begin timer
+        Console.WriteLine("\nPlease enter 'start' to begin stopwatch.");
+       
+        
+        
+            
+            var start = Convert.ToString(Console.ReadLine());
+        timer.Start = start;
+        var startTime = DateTime.Now;
+
+        // check to see if input is valid
+        if (timer.Start != "start")
+        {
+            // Throw exception if input != valid
+            throw new System.Exception("Input was not valid.");
+        }
+
+
+        // Prompt user to end timer
+        Console.WriteLine("\nPlease enter 'stop' to stop timer.");
+
+        var stop = Convert.ToString(Console.ReadLine());
+        timer.Stop = stop;
+        var stopTime = DateTime.Now;
+
+        // check to see if input is valid
+        if (timer.Stop == "stop")
+        {
+            // Calculate time passed using TotalTime method
+            Console.WriteLine("Total time is: {0}", timer.TotalTime(startTime, stopTime)); 
+        }
+        else
+        {
+            // Throw exception if input != valid
+            throw new System.Exception("Input was not valid.");
+        }
+
+
+        
+        
+
+
+
+    }
+}
+        
+     
+
---
+++ Timer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+public class Timer
+
+{
+    // Initialize variables
+    private TimeSpan _totalTime;
+
+    public DateTime StartTime;
+    public DateTime EndTime;
+    public string Start { get; set; }
+    public string Stop { get; set; }
+
+    // Constructor that reads "Stopwatch" whenever there is a new instance of the "Timer" class.
+    public Timer()
+    {
+        Console.WriteLine("Stopwatch");        
+
+    }
+
+    // Timespan method that calculates time passed while stopwatch is running
+    public TimeSpan TotalTime(DateTime startTime, DateTime endTime)
+    {
+        this.StartTime = startTime;
+        this.EndTime = endTime;
+
+        // Total time passed is the current time when the stopwatch application is begun subtracted
+        // from the current time when the stopwatch application is terminated.
+        _totalTime = endTime - startTime;
+
+        return _totalTime;
+
+    }
+
+
+    
+
+}
+
